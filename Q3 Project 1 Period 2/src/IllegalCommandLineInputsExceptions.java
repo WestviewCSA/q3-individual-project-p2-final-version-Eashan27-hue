888b27ff5b58ac2c6a629963ebecdcb42610af2b
Eashan27-hue <jhaen2009@gmail.com>
2026-03-11 11:40:25 -0700

    Add IllegalCommandLineInputsException class

---
+++ Q3 Project 1 Period 2/src/IllegalCommandLineInputsExceptions.java
@@ -0,0 +1,8 @@
+
+public class IllegalCommandLineInputsException extends Exception {
+
+	public IllegalCommandLineInputsException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
